4946e536776c85fab0355f6696c4d1fa5b37a45f
Ximing Cheng <chengximing1989@gmail.com>
2014-06-01 00:05:48 +0800

    Add install_package for auto install package

---
+++ tools/install_package.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+# install all need package
+# need the super user permission only support for debian/Ubuntu/Mint
+# author XimingCheng
+
+# linux c/c++ complier with gcc and clang
+apt-get -qq install build-essential
+apt-get -qq install clang libclang-dev
+
+# linux cmake tools
+apt-get -qq install cmake
+
+# git subversion source ctrl
+apt-get -qq install git subversion
+
+# wxWidegets dev
+apt-get -qq install libx11-dev libwxbase2.8-dev libwxgtk2.8-dev libwxsqlite3-2.8-dev
+
+# vim (ruby support)
+apt-get -qq install vim-nox
+
+# java
+apt-get -qq install openjdk-7-jdk
+
+# utiles
+apt-get -qq install htop
+apt-get -qq install upower
+apt-get -qq install vlc
+apt-get -qq install tmux
+
+apt-get -qq install zsh
+
+# more package will be added in the future
